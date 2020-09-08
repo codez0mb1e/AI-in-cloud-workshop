9ee5b41f21fa17e570fe4be8d3cbc13e2828ba0e
codez0mb1e <d.petukhov@outlook.com>
2020-09-08 16:25:38 +0000

    Update init.sh

--- azure-ml-service/init.sh
+++ azure-ml-service/init.sh
@@ -11,8 +11,13 @@ python --version
 pip --version
 
 
+# install dependencies
+pip install -r requirements.txt
+
+
 # clone this repo
 git clone https://github.com/codez0mb1e/AI-in-cloud-workshop.git
+
 
 # and switch to nessesary branch
 cd AI-in-cloud-workshop/
